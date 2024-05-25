412b645a44fc40dc35211a5adc74952c7dff279d
alberto.paro <alberto.paro@gmail.com>
2024-05-25 14:27:14 +0200

    Better header management

--- tests/unit.rs
+++ tests/unit.rs
@@ -17,41 +17,53 @@ fn test_anchor() {
 
 #[test]
 fn test_anchor2() {
-    let md = parse_html(r#"<p><a href="http://ya.ru">APOSIMZ</a><a href="http://yandex.ru">SIDONIA</a></p>"#);
+    let md = parse_html(
+        r#"<p><a href="http://ya.ru">APOSIMZ</a><a href="http://yandex.ru">SIDONIA</a></p>"#,
+    );
     assert_eq!(md, "[APOSIMZ](http://ya.ru)[SIDONIA](http://yandex.ru)")
 }
 
 #[test]
 fn test_anchor3() {
-    let md = parse_html(r#"<p><a href="http://ya.ru">APOSIMZ</a><p/><a href="http://yandex.ru">SIDONIA</a></p>"#);
-    assert_eq!(md, "\
+    let md = parse_html(
+        r#"<p><a href="http://ya.ru">APOSIMZ</a><p/><a href="http://yandex.ru">SIDONIA</a></p>"#,
+    );
+    assert_eq!(
+        md,
+        "\
 [APOSIMZ](http://ya.ru)
 
-[SIDONIA](http://yandex.ru)")
+[SIDONIA](http://yandex.ru)"
+    )
 }
 
 #[test]
 fn test_image() {
-    let md = parse_html(r#"<p><a href="https://gitter.im/MARC-FS/Lobby?utm_source=badge&amp;utm_medium=badge&amp;utm_campaign=pr-badge&amp;utm_content=badge"><img src="https://img.shields.io/gitter/room/MARC-FS/MARC-FS.svg" alt="Gitter"></a><br>"#);
+    let md = parse_html(
+        r#"<p><a href="https://gitter.im/MARC-FS/Lobby?utm_source=badge&amp;utm_medium=badge&amp;utm_campaign=pr-badge&amp;utm_content=badge"><img src="https://img.shields.io/gitter/room/MARC-FS/MARC-FS.svg" alt="Gitter"></a><br>"#,
+    );
     assert_eq!(md, "[![Gitter](https://img.shields.io/gitter/room/MARC-FS/MARC-FS.svg)](https://gitter.im/MARC-FS/Lobby?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge&utm_content=badge)")
 }
 
 #[test]
 fn test_escaping() {
     let md = parse_html(r#"<p>*god*'s in his **heaven** - all is right with the __world__</p>"#);
-    assert_eq!(md, "\\*god\\*\'s in his \\*\\*heaven\\*\\* - all is right with the \\_\\_world\\_\\_")
+    assert_eq!(
+        md,
+        "\\*god\\*\'s in his \\*\\*heaven\\*\\* - all is right with the \\_\\_world\\_\\_"
+    )
 }
 
 #[test]
 fn test_escaping_mid_hyphens() {
     let md = parse_html(r#"<h1>This is a header with-hyphen!</h1>"#);
-    assert_eq!(md, "This is a header with-hyphen!\n==========")
+    assert_eq!(md, "# This is a header with-hyphen!")
 }
 
 #[test]
 fn test_escaping_start_hyphens() {
     let md = parse_html(r#"<h1>- This is a header with starting hyphen!</h1>"#);
-    assert_eq!(md, "\\- This is a header with starting hyphen!\n==========")
+    assert_eq!(md, "# - This is a header with starting hyphen!")
 }
 
 #[test]
@@ -64,26 +76,33 @@ fn test_escaping_start_sharp() {
 #[test]
 fn test_escaping_start_hyphens_space() {
     let md = parse_html(r#"<h1>   - This is a header with starting hyphen!</h1>"#);
-    assert_eq!(md, " \\- This is a header with starting hyphen!\n==========")
+    assert_eq!(md, "#  - This is a header with starting hyphen!")
 }
 
 #[test]
 fn test_escaping_html_tags() {
-    let md = parse_html(r#"xxxxxxx xx xxxxxxxxxxx: &lt;iframe src="xxxxxx_xx_xxxxxxxxxxx/embed/" allowfullscreen="" height="725" width="450"&gt;&lt;/iframe&gt;"#);
-    assert_eq!(md, r#"xxxxxxx xx xxxxxxxxxxx: \<iframe src="xxxxxx\_xx\_xxxxxxxxxxx/embed/" allowfullscreen="" height="725" width="450"\>\</iframe\>"#)
+    let md = parse_html(
+        r#"xxxxxxx xx xxxxxxxxxxx: &lt;iframe src="xxxxxx_xx_xxxxxxxxxxx/embed/" allowfullscreen="" height="725" width="450"&gt;&lt;/iframe&gt;"#,
+    );
+    assert_eq!(
+        md,
+        r#"xxxxxxx xx xxxxxxxxxxx: \<iframe src="xxxxxx\_xx\_xxxxxxxxxxx/embed/" allowfullscreen="" height="725" width="450"\>\</iframe\>"#
+    )
 }
 
 #[test]
 fn test_headers() {
-    let md = parse_html(r#"<h1 id="marc-fs">MARC-FS</h1><p><a href="http://Mail.ru">Mail.ru</a> Cloud filesystem written for FUSE</p><h2 id="synopsis">Synopsis</h2>"#);
-    assert_eq!(md, "\
-MARC-FS
-==========
+    let md = parse_html(
+        r#"<h1 id="marc-fs">MARC-FS</h1><p><a href="http://Mail.ru">Mail.ru</a> Cloud filesystem written for FUSE</p><h2 id="synopsis">Synopsis</h2>"#,
+    );
+    assert_eq!(
+        md,
+        "# MARC-FS
 
 [Mail.ru](http://Mail.ru) Cloud filesystem written for FUSE
 
-Synopsis
-----------")
+## Synopsis"
+    )
 }
 
 #[test]
